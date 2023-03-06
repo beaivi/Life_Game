546b8b8145f22f7165696f80c2d4eaa4d77a3a75
alisjo <113341221+alisjo@users.noreply.github.com>
2023-03-06 10:43:59 +0100

    Add files via upload

---
+++ menus.ts
@@ -0,0 +1,218 @@
+import { type User, type Task, input } from "./types";
+import { change_password, change_username, login, create_user } from "./user";
+import { preset, add_task, reset_tasks, complete_tasks, remove_task  } from "./tasks";
+
+/**
+ * Shows all tasks the user has added, 
+ * taking into account the frequency of the task.
+ * @param user the user with the tasks shown
+ */
+export function show_tasks_menu(user: User): void {
+    function show_tasks_with_freq(taskarray: Array<Task>): void {
+        function show_freq(taskarray: Array<Task>, freq: string) {
+            if(taskarray.length !== 0) {
+                console.log("\n" + freq);
+                for(let i = 0; i < taskarray.length; i++){
+                    console.log("   " + taskarray[i].name);
+                }
+            }
+        }
+        const daily_tasks: Array<Task> = [];
+        const weekly_tasks: Array<Task> = [];
+        const monthly_tasks: Array<Task> = [];
+        for(let i = 0; i < taskarray.length; i++) {
+            if(taskarray[i].freq === "daily"){
+                daily_tasks.push(taskarray[i]);
+            } else if(taskarray[i].freq === "weekly") {
+                weekly_tasks.push(taskarray[i]);
+            } else if(taskarray[i].freq === "monthly") {
+                monthly_tasks.push(taskarray[i]);
+            } else {}
+        }
+        show_freq(daily_tasks, "  Daily tasks: ");
+        show_freq(weekly_tasks, "  Weekly tasks: ");
+        show_freq(monthly_tasks, "  Monthly tasks: ");
+    }
+    function show_tasks_status(user: User, completed: boolean) {
+        let tasks_with_curr_status: Array<Task> = [];
+        for(let i = 0; i < user.tasks.length; i++){
+            if(user.tasks[i].status === completed) {
+                tasks_with_curr_status.push(user.tasks[i]);
+            }
+        }
+        if(tasks_with_curr_status.length === 0) {
+            if(completed) {
+                console.log("\nYou have not completed any tasks yet.");
+            } else {
+                console.log("\nYou have completed all your tasks");
+            }
+        } else {
+            if(completed) {
+                console.log("\nYou have completed the following tasks: ");
+            } else {
+                console.log("\nYou have the following tasks left to do: ");
+            }
+                show_tasks_with_freq(tasks_with_curr_status);
+            }
+    }
+    if (user.tasks.length === 0) {
+        console.log("You don't have any tasks");
+    } else {
+        while(true) {
+            console.log("What do you want to do? \n a) Show all tasks\n" +
+                        " b) Show tasks left to do\n c) " + 
+                        " Show completed tasks\n x) Back to main menu");
+            let choice: string = input("Choose a, b or c: ");
+            choice = choice.toLowerCase();
+            if(choice === "a"){
+                console.log("\nYou have the following tasks: ");
+                show_tasks_with_freq(user.tasks);
+                break;
+            } else if(choice === "b") {
+                show_tasks_status(user, false);
+                break;
+            } else if(choice === "c") {
+                show_tasks_status(user, true);
+                break;
+            } else if(choice === "x") {
+                back_to_menu();
+            }
+            else {
+                console.log("\nWrong input");
+            }
+        }
+    }
+}
+
+/**
+ * Displays a menu over the settings available and lets the user decide
+ * what do do.
+ * @param user the user loged in
+ */
+export function settings_menu(user: User): void {
+    console.log("What do you want to do?");
+    console.log("\n a) Change password \n b) Change username\n" + 
+                " x) Back to main menu ");
+    let have_chosen = false;
+    while(!have_chosen){
+        let choice: string = input("Choose a or b: ");
+        choice.toLowerCase();
+        if(choice === "a"){
+            change_password(user);
+            have_chosen = true;
+        } else if(choice === "b"){
+            change_username(user);
+            have_chosen = true;
+        } else if (choice === "x"){
+            back_to_menu();
+        } else {
+            console.log("\nWrong input\n");
+        }
+    }
+}
+
+export let active_user: User | undefined = undefined;
+
+export function log_in_menu() {
+    while (active_user === undefined) {
+        console.log("\nDo you want to: \n a). register \n b). log in? \n " + 
+                    " x). Exit out of program?")
+        let choice: string = input("Choose a, b or x. ")
+        choice = choice?.toLowerCase();
+        if (choice === "a") {
+            active_user = create_user();
+            console.log("\nYou have successfully created a new user.\n")
+            preset(active_user);
+        } else if (choice === "b") {
+            active_user = login();
+        } else if (choice === "x") {
+            break;
+        } 
+
+        else {
+            console.log("\nWrong input, try again. \n")
+        }
+    }
+}
+
+/**
+ * Lets the user decide what they want to do with their tasks
+ * @param user the user logged in
+ */
+export function task_edit_menu(user: User): void {
+    console.log("\nWhat do you want do do?");
+    console.log("\n a) Add tasks \n b) Remove tasks \n c) Reset time\n " + 
+                "x) Back to main menu\n");
+    let have_chosen: boolean = false;
+    console.log("");
+    while(!have_chosen) {
+        let choice: string = input("Choose a, b, c or x: ");
+        choice = choice?.toLowerCase();
+        if(choice === "a") {
+            add_task(user);
+            have_chosen = true;
+        } else if(choice === "b") {
+            remove_task(user);
+            have_chosen = true;
+        } else if(choice === "c") {
+            reset_tasks(user);
+            have_chosen = true;
+        } else if (choice === "x"){
+            back_to_menu();
+        }
+        else {
+            console.log("Wrong input");
+        }
+    }
+}
+
+export function back_to_menu(): void {
+    main_menu();
+}
+
+/**
+ * 
+ * 
+ * 
+**/
+export function main_menu(): void {
+    let exit: boolean = false;
+    while (!exit) {
+        if (active_user === undefined) {
+            log_in_menu();
+        }   
+        if (active_user !== undefined) {
+            console.log("\nWhat do you want to do?")
+            console.log("\n a) Add or edit tasks \n b) Complete tasks \n" 
+                      + " c) Show your points \n" +
+                        " d) Show your tasks \n e) Log out \n f) Settings \n" + 
+                        " x) Exit out of program.")
+            let choice: string = input("Choose a, b, c, d, e ,f or x: ");
+            choice = choice?.toLowerCase();
+            console.log("");
+            if(choice === "a"){
+                task_edit_menu(active_user);
+            } else if(choice === "b"){
+                complete_tasks(active_user);
+            } else if(choice === "c"){
+                const level = (x: User)  => x.level;
+                const score = (x: User) => x.score;
+                console.log("Your level is ", level(active_user), + 
+                            "\nand your score is:", score(active_user));
+            } else if (choice === "d"){
+                show_tasks_menu(active_user);
+            } else if (choice === "e"){
+                active_user = undefined;
+            } else if (choice === "f"){
+                settings_menu(active_user);
+            } else if (choice === "x") {
+                exit = true;
+            }
+            else {
+                console.clear();
+                console.log("Wrong input");
+            }
+        } 
+    }
+
+}
---
+++ user.ts
@@ -0,0 +1,145 @@
+import { type User, input, user_table } from "./types";
+import { ph_lookup, ph_insert, ph_delete } from "./lib/hashtables";
+import {back_to_menu} from "./menus";
+
+/**
+ * Creates a new user with username and password inputs.
+ * @returns the new user created
+ */
+export function create_user(): User {
+    function add_user(name: string): User {
+        return {
+            username: name,
+            password: "",
+            tasks: [],
+            score: 0,
+            level: 1
+
+        };
+    }
+    let created_user: User | undefined = undefined;
+    let password: string = "";
+    let username: string  = "";
+
+    while (created_user === undefined) {
+        username = input("Choose an username: ");
+        if (ph_lookup(user_table, username) === undefined) {
+            console.log("Your username was available.\n");
+            ph_insert(user_table, username, add_user(username));
+            created_user = ph_lookup(user_table, username);
+        }
+        else {
+            console.log("Your usernames was unavailable, please " + 
+                        " try another one. ");
+        }
+    }
+    const user: User | undefined = ph_lookup(user_table, username)
+
+    while (password?.length < 6) {
+        password = input("Choose a password for your user: ");
+        if (password?.length < 6) {
+            console.log("The password should be atleast 6 characters long. ")
+        }
+        else {
+            console.log("You have successfully chosen a password.")
+            if (user !== undefined) {
+                user.password = password;
+            } else {
+                create_user();
+            }
+        }
+    }
+    return created_user;
+}
+
+/**
+ * Lets the user change their username.
+ * @param user the user that gets its username changed
+ */
+export function change_username(user: User): void {
+    let username_available: boolean = false;
+    while(!username_available) {
+        let new_username: string = input("New username: ")
+        if (ph_lookup(user_table, new_username) === undefined) {
+            ph_delete(user_table, user.username);
+            user.username = new_username;
+            ph_insert(user_table, user.username, user);
+            console.log("\nYou have sucessfully changed your username\n");
+            username_available = true;
+        } else {
+            console.log("Your username was unavailable, please" + 
+            " try another one. ");
+            
+        }
+    
+    }
+}
+
+/**
+ * Lets the user change their password
+ * @param user that gets its password changed
+ */
+export function change_password(user: User): void {
+    let old_password_ok: boolean = false;
+    while(!old_password_ok){
+        const old_password: string = input("Old password: ")
+
+        if(old_password === user.password){
+            let password_match: boolean = false;
+            while(!password_match){
+                let new_password: string = input("New password: ")
+                while(new_password?.length < 6) {
+                if(new_password?.length < 6) {
+                    console.log("\nThe password should be atleast 6" + 
+                    "characters long.");
+                    new_password = input("New password: ");
+                }
+                }
+                const confirmed_password: string = input("Confirm your new" + 
+                " password: ");
+                if(new_password === confirmed_password){
+                    user.password = new_password;
+                    console.log("\nYou have sucessfully changed your" + 
+                    " password\n");
+                    password_match = true;
+                } else {
+                    console.log("\nPassword confirmation doesn't match "+ 
+                    " the password, try again\n")
+                }
+            }
+            old_password_ok = true;
+        } else {
+            console.log("Wrong password, try again");
+        }
+    }
+}
+
+/**
+ * Login a user based on username and password, creates a new user if user
+ * is not registerd already
+ * @returns the user currently logged in, or @undefined if 
+ * the user is not registered.
+ */
+export function login(): User | undefined { 
+    const curr_username: string = input("Username: ");
+    const curr_user = ph_lookup(user_table, curr_username);
+    if(curr_user !== undefined) {
+        let pass_input: boolean = false;
+        while(!pass_input) {
+            const password: string = input("Password: ");
+            if(curr_user?.password === password) {
+                pass_input = true;
+                console.log("\nYou have successfully logged in.\n");
+                return curr_user;
+            } else if (password === "x") {
+                back_to_menu();
+            } else {
+                console.log("\nWrong Password, try again\n If you want to go" +
+                " back to the login menu write 'x'");
+            }
+        }
+    } else {
+        console.log("Username not found");
+        return undefined;
+    }
+}
